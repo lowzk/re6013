a53542b12626b6efdc9cecb4550cf74a40a4e27c
Low Zhe Kai <lowzhekai69@gmail.com>
2025-01-21 15:16:24 +0800

    Add index.html and css

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask, request, render_template
+from test import print_internal_name
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    print_internal_name()
+    app.run()
---
+++ test.py
@@ -0,0 +1,2 @@
+def print_internal_name():
+    print(__name__)
